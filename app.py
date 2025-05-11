1e435d92b5dfc53f2f51e7c82b29a4c1d070ae84
Ruben Günther <rubengunther@Mac-mini-von-Ruben.local>
2025-05-11 12:16:54 +0200

    Fix: richtiger Zugriff auf bestehenden Dropbox-Link

--- app.py
+++ app.py
@@ -94,14 +94,8 @@ def search_dropbox_videos(name, pin):
 
                 url = url.replace("?dl=0", "?dl=1")
                 found_links.append((entry.name, url))
-            except dropbox.exceptions.ApiError as e:
-                try:
-                    if e.error.is_shared_link_already_exists():
-                        url = e.error.get_shared_link_already_exists().url
-                        url = url.replace("?dl=0", "?dl=1")
-                        found_links.append((entry.name, url))
-                except Exception as inner:
-                    print(f"Fehler beim Link-Generieren für {entry.name}: {inner}", file=sys.stderr)
+            except Exception as e:
+                print(f"Fehler beim Link-Generieren für {entry.name}: {e}", file=sys.stderr)
 
         print(f"Matches: {found_links}", file=sys.stderr)
     except Exception as e:
